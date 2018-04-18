57fd0f97d8b9247305e1ef87c96ae1e0ad765a6f
Ken Mutch <palladyne@gmail.com>
2018-04-18 04:12:43 +0000

    added stub delete function, changed path to '/selected-shows'

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/selected-shows', (req, res) => {
   res.send({
     "Output": "Hello World!"
   });
 });
 
-app.post('/', function(req, res) {
+app.post('/selected-shows', (req, res) => {
   res.send({
     "Output": "Hello World!"
   });
 });
+
+app.delete('/selected-shows/:id', (req, res) => {
+  res.send({
+    "Output": "This will delete selected show with id of " + req.params.id
+  })
+})
 
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
--- tests/test.js
+++ tests/test.js
@@ -8,15 +8,22 @@ const request = supertest(app);
 
 describe('Tests app', function() {
   it('verifies get', function(done) {
-    request.get('/').expect(200).end(function(err, result) {
+    request.get('/selected-shows').expect(200).end(function(err, result) {
         test.string(result.body.Output).contains('Hello');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
   });
   it('verifies post', function(done) {
-    request.post('/').expect(200).end(function(err, result) {
+    request.post('/selected-shows').expect(200).end(function(err, result) {
         test.string(result.body.Output).contains('Hello');
+        test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
+        done(err);
+    });
+  });
+  it('verifies delete', function(done) {
+    request.delete('/selected-shows/1').expect(200).end(function(err, result) {
+        test.string(result.body.Output).contains('id of 1');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
