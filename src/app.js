7aa167d397f3150e687ff06c48a4906b21e77370
Ken Mutch <ken.mutch@team.telstra.com>
2018-04-20 07:20:45 +0000

    testing awsServerlessExpressMiddleware.eventContext()

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var authParser  = require('./aws-apigw-cognito-user-auth-parser');
-var _get = require('lodash.get');
-var app = express();
+const express = require('express');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const authParser  = require('./aws-apigw-cognito-user-auth-parser');
+const _get = require('lodash.get');
+const app = express();
 
-
-app.use(authParser({}));
+app.use(authParser());
+app.use(awsServerlessExpressMiddleware.eventContext())
 
 app.get('/selected-shows', (req, res) => {
   res.send({
-    "Output": "Hello " + _get(req, 'authContext.claims.username', 'anonymous')
+    "Output": "Hello " + _get(req, 'authContext.claims.username', 'anonymous'),
+    "ApiGwEvent": _get(req, 'apiGateway.event', 'no gateway event found')
   });
 });
 
--- src/aws-apigw-cognito-user-auth-parser.js
+++ src/aws-apigw-cognito-user-auth-parser.js
@@ -1,15 +1,14 @@
 urldecode = require('urldecode');
 module.exports = function(options) {
     return function(req, res, next) {
-        
         try {
             const gatewayEvent = JSON.parse(urldecode(req.headers['x-apigateway-event']));
-             var authContext = gatewayEvent.requestContext.authorizer
+            var authContext = gatewayEvent.requestContext.authorizer
             req.authContext = authContext
             next();
         }
         catch(e) {
-            console.error(e);
+            console.error()
             next();
         }
     }
