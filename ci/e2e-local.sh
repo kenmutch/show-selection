4ffbc8b237cc51efe9e501783d9cb9a0578917f1
Ken Mutch <ken.mutch@team.telstra.com>
2018-04-28 05:09:46 +0000

    fix to table definition in template.yml

--- ci/e2e-local.sh
+++ ci/e2e-local.sh
@@ -5,5 +5,5 @@ export AWS_ACCESS_KEY_ID=$(grep -i "aws_access_key_id" ~/.aws/credentials | cut 
 export AWS_SECRET_ACCESSKEY=$(grep -i "aws_secret_access_key" ~/.aws/credentials | cut -d "=" -f 2)
 export REGION='ap-southeast-2'
 export TABLE_NAME='ShowSelectionsLocal'
-#docker run -d -p 8000:8000 --name dynamodb-local dmwkerr/dynamodb
+docker run -d -p 8000:8000 --name dynamodb-local dmwkerr/dynamodb
 ./node_modules/.bin/mocha --recursive e2e
