fc007d0e93ac99e2bbc14de4a92ae1b85c6fd905
Ken Mutch <palladyne@gmail.com>
2018-04-29 08:46:27 +0000

    adding logging to investigate NPE

--- src/aws-apigw-cognito-user-auth-parser.js
+++ src/aws-apigw-cognito-user-auth-parser.js
@@ -2,6 +2,7 @@ urldecode = require('urldecode');
 module.exports = function(options) {
     return function(req, res, next) {
         try {
+            console.log('req.header', req.headers);
             const gatewayEvent = JSON.parse(urldecode(req.headers['x-apigateway-event']));
             var authContext = gatewayEvent.requestContext.authorizer
             req.authContext = authContext
