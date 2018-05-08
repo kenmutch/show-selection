feddc77bbd703be12711ee68d895a7078add5ab2
Ken Mutch <ken.mutch@team.telstra.com>
2018-05-08 07:53:30 +0000

    Added bunyan logging to auth-parser

--- src/aws-apigw-cognito-user-auth-parser.js
+++ src/aws-apigw-cognito-user-auth-parser.js
@@ -1,14 +1,17 @@
 urldecode = require('urldecode');
+const logger = require('./bunyan-log-provider').getLogger();
+
 module.exports = function(options) {
     return function(req, res, next) {
         try {
             const gatewayEvent = JSON.parse(urldecode(req.headers['x-apigateway-event']));
             var authContext = gatewayEvent.requestContext.authorizer
             req.authContext = authContext
+            logger.debug(`Set authcontext onto request: ${JSON.stringify(authContext)}`);
             next();
         }
         catch(e) {
-            console.error(e)
+            logger.error(e)
             next();
         }
     }
