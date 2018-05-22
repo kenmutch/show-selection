0904ee94f1044e1d0f454ceb95a7d61c8d1f7753
Ken Mutch <palladyne@gmail.com>
2018-05-22 11:57:32 +0000

    fix placement of logical ID in template outputs

--- ci/e2e-local.sh
+++ ci/e2e-local.sh
@@ -24,11 +24,20 @@ SNS_CONTAINER='snssqs-mock'
 
 if ! docker ps | grep -q $DYNAMODB_CONTAINER; then
     echo "container '$DYNAMODB_CONTAINER' is not running, so starting it"
-    docker start $DYNAMODB_CONTAINER
+    if ! docker images | grep -q "dwmkerr/dynamodb"; then
+        docker run -d -p 8000:8000 --name $DYNAMODB_CONTAINER dwmkerr/dynamodb
+    else
+        docker start $DYNAMODB_CONTAINER
+    fi
 fi
 if ! docker ps | grep -q $SNS_CONTAINER; then
     echo "container '$SNS_CONTAINER' is not running, so starting it"
-    docker start $SNS_CONTAINER
+    if ! docker images | grep -q "zix99/snssqs-mock"; then
+        docker run -d -p 9325:9325 -p 9324:9324 --name $SNS_CONTAINER zix99/snssqs-mock:latest
+    else
+        docker start $SNS_CONTAINER
+    fi
 fi
 
 ./node_modules/.bin/mocha --recursive e2e | ./node_modules/.bin/bunyan
+
